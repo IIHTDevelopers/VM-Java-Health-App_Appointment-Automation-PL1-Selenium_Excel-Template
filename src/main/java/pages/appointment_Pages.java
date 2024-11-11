2c06049a151cfc9b78b8dfeca80a5fbd55f3cd64
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-11-11 18:31:15 +0530

    Add files via upload

--- src/main/java/pages/appointment_Pages.java
+++ src/main/java/pages/appointment_Pages.java
@@ -7,7 +7,98 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class appointment_Pages extends StartupPage {
+	
+	//Locators of L1 Scenarios
+	//login locators
+	By usernameTextfield = By.id(" ");
+	By usernameTextbox = By.xpath(" ");
+	By passwordTextbox = By.xpath(" ");
+	By signInButton = By.xpath(" ");
+	By registeredPatientTextElement = By.xpath(" ");
 
+	//Appoint Module's locators 
+	By appointmentModuleByElement = By.xpath(" ");
+	By appointmentModuleToggleByElement = By.xpath(" ");
+	By selectCounterTitleNameByElement = By.xpath(" ");
+
+	By selectCounterNewOneLinkByElement = By.xpath(" ");
+	By newPatientButtonLinkByElement = By.xpath(" ");
+	By patientInformationTextByElement = By.xpath(" ");
+	By careOfPersonContactTextboxByElement = By.xpath(" ");
+	By careOfPersonTextboxByElement = By.xpath(" ");
+	By printInvoiceButtonByElement = By.xpath(" ");
+	By confirmButtonByElement = By.xpath(" ");
+	By errorMessageOfLastNameTextboxByElement = By.xpath(" ");
+
+	By firstNameTextboxByElement = By.xpath(" ");
+	By middleNameTextboxByElement = By.xpath(" ");
+	By lastNameTextboxByElement = By.xpath(" ");
+	By ageTextboxByElement = By.xpath(" ");
+	By phoneTextboxByElement = By.xpath(" ");
+
+	By havDOBCheckboxByElement = By.xpath(" ");
+	By dobDatePickerByElement = By.xpath(" ");
+
+	By externalCheckBoxByElement = By.xpath(" ");
+	By plusIconClickToAddExternalReffererByElement = By.xpath(" ");
+	By addExternalReferralTitleByElement = By.xpath(" ");
+	By referrerNameTexboxByElement = By.xpath(" ");
+	By extRefferAddressTextFieldElement = By.xpath(" ");
+	By contactNumberTextFieldElement = By.xpath(" ");
+	By emailTextFieldElement = By.xpath(" ");
+	By isIncentiveApplicableCheckboxElement = By.xpath(" ");
+	By addreferrerButtonByElement = By.xpath(" ");
+	By successNotificationPopupMessageElement = By.xpath(" ");
+	By successNotificationPopupMessageOneElement = By.xpath(" ");
+	By successNotificationPopupMessageTwoElement = By.xpath(" ");
+
+	By relationWithPatientDropdownByElement = By.xpath(" ");
+
+	By bookAppointmentSubModuleByElement = By.xpath(" ");
+	By newPatientButtonByElement = By.xpath(" ");
+
+	By monthRadioButtonByElement = By.xpath(" ");
+	By daysRadioButtonByElement = By.xpath(" ");
+
+	By firstNameTexboxOfBookAppointmentByElement = By.xpath(" ");
+	By middleNameTexboxOfBookAppointmentByElement = By.xpath(" ");
+	By lastNameTexboxOfBookAppointmentByElement = By.xpath(" ");
+	By genderMaleOptionOfBookAppointmentByElement = By.xpath(" ");
+	By addAppointmentButtonByElement = By.xpath(" ");
+
+	By errorNotificationPopupMessageElement = By.xpath(" ");
+	By statusErrorNotificationMessageByElement = By.xpath(" ");
+	By mainErrorNotificationMessageByElement = By.xpath(" ");
+
+	By procurementModule = By.xpath(" ");
+	By settingsTab = By.xpath(" ");
+	By subCategoryTabByElement = By.xpath(" ");
+	By cuttonSubCategoryNameByElement = By.xpath(" ");
+
+	By vaccinationModuleByElement = By.xpath(" ");
+	By vaccinationModulesToggleIconByElement = By.xpath(" ");
+	By patientListSubModuleByElement = By.xpath(" ");
+	By titleNameOfVaccinationPatientRegisterFormByElement = By.xpath(" ");
+	By newVaccinationPatientButtonByElement = By.xpath(" ");
+
+	By keyboardButtonElement = By.xpath(" ");
+	By altPlusNtooltipMessageElement = By.xpath(" ");
+
+	By membershipDropdownByElement = By.xpath(" ");
+
+	By reportsSubButton = By.xpath(" ");
+	By currentStockLevelButton = By.xpath(" ");
+	By selectedItemDropdown = By.xpath(" ");
+	By selectItemSearchBar = By.xpath(" ");
+	By accountsItemCheckBox = By.xpath(" ");
+	By generalInventoryOption = By.xpath(" ");
+	By accountOption = By.xpath(" ");
+
+	By invoiceHeadersSubTab = By.xpath(" ");
+	By addNewInvoiceHeaderButton = By.xpath(" ");
+	By chooseFileButton = By.id(" ");
+	By closeAddNewPatientForm = By.xpath(" ");
+	By xButton = By.xpath(" ");
 	
 	String pageName = this.getClass().getSimpleName();
 	public appointment_Pages(WebDriver driver) {
